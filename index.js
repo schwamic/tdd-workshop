91df86ddca43a058cbff3c5220712bc5b5ab8f63
Michael Schwarz <michael.schwarz@xitaso.com>
2022-02-25 21:07:10 +0100

    update tests

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 class Cell {
-    constructor(state=true, neighbours=[0,0,0,0,0,0,0,0]) {
+    constructor(state=1, neighbours=[0,0,0,0,0,0,0,0]) {
         this.state = state;
         this.neighbours = neighbours
     }
 
+    _getAmountOfNeighbours(){
+        return this.neighbours.reduce((prev, curr) => prev + curr)
+    }
+
     isAlive(){
-        return this.state;
+        return !!this.state;
+    }
+
+    willSurvive(){
+        const activeNeighbours = this._getAmountOfNeighbours()
+
+        // Death Cases
+        if(activeNeighbours < 2 || activeNeighbours > 3){
+            return false;
+        }
+
+        return true;
     }
 }
-
 
 module.exports = {
     Cell
--- index.spec.js
+++ index.spec.js
@@ -1,11 +1,18 @@
 const { Cell } = require('./index');
 
-test('Create default cell', () => {
+test('Create cell', () => {
     const cell = new Cell();
-    expect(cell.state).toBe(true);
+    expect(cell.isAlive()).toBe(true);
 });
 
-test('Create dead cell', () => {
-    const cell = new Cell(false);
-    expect(cell.state).toBe(false);
+test('Cell will die if less then two neighbours', () => {
+    const neighbours = [0,0,0,0,1,0,0,0];
+    const cell = new Cell(true, neighbours);
+    expect(cell.willSurvive()).toBe(false);
+});
+
+test('Cell will die if more then three neighbours', () => {
+    const neighbours = [1,0,1,0,1,1,0,0];
+    const cell = new Cell(true, neighbours);
+    expect(cell.willSurvive()).toBe(false);
 });
